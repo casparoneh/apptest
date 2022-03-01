453227290d16225120823c2ecbb5c3468b074308
casparoneh <yamada_lori@hotmail.com>
2022-03-01 23:40:43 +0100

    updateeee4

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -3,12 +3,15 @@ import LastProducts from "./LastProducts";
 
 const Sidebar = ({ categories, lastsPosts }) => {
   return (
-    <div>
+    <>
+      <div>
       <Categories categories={categories} />
       <div className="mt-8">
         <LastProducts lastsPosts={lastsPosts} />
       </div>
     </div>
+    </>
+  
   );
 };
 
